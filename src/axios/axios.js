ccaee0a00dd40b19b22edf26b48fa9af2d9975f0
rizkiismail <laksmimentari@gmail.com>
2023-09-05 20:24:11 +0700

    trying to configure 1

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 export const axiosInstace = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://misty-puce-hedgehog.cyclic.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 export const axiosPrivateInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://misty-puce-hedgehog.cyclic.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 export const axiosPrivateImageInstance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: "https://misty-puce-hedgehog.cyclic.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "multipart/form-data",
